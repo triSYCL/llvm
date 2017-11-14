b5d003887923ada952b930b9ae69ae322080c83f
Lin Ya Yu <yu810226@gmail.com>
2017-11-14 16:30:05 +0000

    Set metadata for reqd_work_group_size (1,1,1)
    Set metadata for reqd_work_group_size (1,1,1) while compiling for
    FPGA.
    Add -reqd-workgroup-size-fpga to control wheter adding this metadata or not.

--- lib/SYCL/inSPIRation.cpp
+++ lib/SYCL/inSPIRation.cpp
@@ -43,6 +43,8 @@ using namespace llvm;
 STATISTIC(SYCLKernelProcessed, "Number of SYCL kernel functions processed");
 STATISTIC(SYCLFuncCalledInKernelFound, "Number of functions directly or indirectly called by SYCL kernel functions");
 
+cl::opt<bool> ReqdWorkGroupSizeFPGA("reqd-workgroup-size-fpga", cl::desc("set reqd_work_group_size to be 1-1-1"));
+
 // Put the code in an anonymous namespace to avoid polluting the global
 // namespace
 namespace {
@@ -130,6 +132,8 @@ struct inSPIRation : public ModulePass {
     SmallVector<llvm::Metadata *, 8> TypeQuals;
     // MDNode for the kernel argument access qualifiers
     SmallVector<llvm::Metadata *, 8> AccessQuals;
+    // MDNode for the kernel required work group size
+    SmallVector<llvm::Metadata *, 8> ReqdWorkGroupSize;
 
     for (auto &A : F.args()) {
       std::string TypeName;
@@ -174,6 +178,13 @@ struct inSPIRation : public ModulePass {
             llvm::ConstantAsMetadata::get(
                 llvm::ConstantInt::get(Int32Ty, 0)));
     }
+
+    for (int i = 0; i < 3; ++i) {
+      ReqdWorkGroupSize.push_back(
+          llvm::ConstantAsMetadata::get(
+              llvm::ConstantInt::get(Int32Ty, 1)));
+    }
+
     // Add the SPIR metadata describing the address space of each argument
     F.setMetadata("kernel_arg_addr_space",
                   llvm::MDNode::get(Ctx, AddressSpaceQuals));
@@ -191,6 +202,12 @@ struct inSPIRation : public ModulePass {
     // Add the SPIR metadata describing the access qualifier of each argument
     F.setMetadata("kernel_arg_access_qual",
                   llvm::MDNode::get(Ctx, AccessQuals));
+
+    if (ReqdWorkGroupSizeFPGA) {
+      // Add the SPIR metadata for required work group size
+      F.setMetadata("reqd_work_group_size",
+                    llvm::MDNode::get(Ctx, ReqdWorkGroupSize));
+    }
   }
 
 
